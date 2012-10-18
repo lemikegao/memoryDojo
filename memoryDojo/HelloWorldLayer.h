b299dbbc80de3fd66f214c0c48003756dc916cac
Michael Gao <mgao.ucla@gmail.com>
2012-10-18 12:21:49 -0700

    set up project with ARC template; currently displays "hello world"

---
+++ memoryDojo/HelloWorldLayer.h
@@ -0,0 +1,20 @@
+//
+//  HelloWorldLayer.h
+//  cocos2d-2.x-ARC-iOS
+//
+//  Created by Steffen Itterheim on 27.04.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
