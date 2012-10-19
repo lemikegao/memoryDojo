2d7c1f2cbb729af6053f57ed157d69d3adaaa77e
Michael Gao <mgao.ucla@gmail.com>
2012-10-19 01:23:22 -0700

    added swipe gesture recognizer to game layer with placeholder selectors

--- memoryDojo/GameLayer.m
+++ memoryDojo/GameLayer.m
@@ -7,8 +7,24 @@
 //
 
 #import "GameLayer.h"
+#import "Constants.h"
+
+@interface GameLayer()
+
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeLeftRecognizer;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeDownRecognizer;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeRightRecognizer;
+@property (nonatomic, strong) UISwipeGestureRecognizer *swipeUpRecognizer;
+-(void)handleLeftSwipe;
+-(void)handleDownSwipe;
+-(void)handleRightSwipe;
+-(void)handleUpSwipe;
+
+@end
 
 @implementation GameLayer
+
+@synthesize swipeLeftRecognizer = _swipeLeftRecognizer;
 
 -(id)init {
     self = [super init];
@@ -18,6 +34,9 @@
         background.position = ccp(screenSize.width/2, screenSize.height/2);
         [self addChild:background];
         
+        // comes before labelAction, otherwise action doesn't run
+        [self scheduleUpdate];
+        
         CCLabelBMFont *gameBeginLabel = [CCLabelBMFont labelWithString:@"Game Start" fntFile:@"SpaceVikingFont.fnt"];
         gameBeginLabel.position = ccp(screenSize.width/2, screenSize.height/2);
         [self addChild:gameBeginLabel];
@@ -26,6 +45,56 @@
     }
     
     return self;
+}
+
+-(void)onEnter {
+    [super onEnter];
+    
+    self.swipeLeftRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleLeftSwipe)];
+    self.swipeLeftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    [[CCDirector sharedDirector].view addGestureRecognizer:self.swipeLeftRecognizer];
+    
+    self.swipeDownRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleDownSwipe)];
+    self.swipeDownRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
+    [[CCDirector sharedDirector].view addGestureRecognizer:self.swipeDownRecognizer];
+    
+    self.swipeRightRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleRightSwipe)];
+    self.swipeRightRecognizer.direction = UISwipeGestureRecognizerDirectionRight;
+    [[CCDirector sharedDirector].view addGestureRecognizer:self.swipeRightRecognizer];
+    
+    self.swipeUpRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleUpSwipe)];
+    self.swipeUpRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
+    [[CCDirector sharedDirector].view addGestureRecognizer:self.swipeUpRecognizer];
+}
+
+-(void)onExit {
+    CCLOG(@"GameLayer->onExit");
+    [super onExit];
+    
+    [[CCDirector sharedDirector].view removeGestureRecognizer:self.swipeLeftRecognizer];
+    [[CCDirector sharedDirector].view removeGestureRecognizer:self.swipeDownRecognizer];
+    [[CCDirector sharedDirector].view removeGestureRecognizer:self.swipeRightRecognizer];
+    [[CCDirector sharedDirector].view removeGestureRecognizer:self.swipeUpRecognizer];
+}
+
+-(void)handleLeftSwipe {
+    CCLOG(@"Left Swipe Detected!");
+}
+
+-(void)handleDownSwipe {
+    CCLOG(@"Down Swipe Detected!");
+}
+
+-(void)handleRightSwipe {
+    CCLOG(@"Right Swipe Detected!");
+}
+
+-(void)handleUpSwipe {
+    CCLOG(@"Up Swipe Detected!");
+}
+
+-(void)update:(ccTime)deltaTime {
+    // placeholder -- scheduleUpdate is indeed working
 }
 
 @end
