37d54c06e476702a7bbf89295d7863c8ce0db331
Michael Gao <mgao.ucla@gmail.com>
2012-10-30 18:29:50 -0700

    Added SD texture atlas; added ninja sprite from frame cache to main menu

--- memoryDojo/MainMenuLayer.m
+++ memoryDojo/MainMenuLayer.m
@@ -21,20 +21,25 @@
 
 @implementation MainMenuLayer
 
-@synthesize mainMenu = _mainMenu;
-
 -(id)init {
     self = [super init];
     if (self != nil) {
+        // load texture atlas
+        [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"mainmenu_art.plist"];
+        CCSpriteBatchNode *mainmenuSpriteBatchNode = [CCSpriteBatchNode batchNodeWithFile:@"mainmenu_art.pvr.ccz"];
+        
         CGSize screenSize = [CCDirector sharedDirector].winSize;
-        CCSprite *background = [CCSprite spriteWithFile:@"MainMenuBackground.png"];
+        CCSprite *background = [CCSprite spriteWithFile:@"mainmenu_bg.png"];
         background.position = ccp(screenSize.width/2, screenSize.height/2);
         [self addChild:background];
-        [self displayMainMenu];
         
-        CCSprite *ninja = [CCSprite spriteWithFile:@"louie_dribble.png"];
-        ninja.position = ccp(screenSize.width/2, screenSize.height * 0.65f);
-        [self addChild:ninja];
+        CCSprite *ninja = [CCSprite spriteWithSpriteFrameName:@"mainmenu_ninja.png"];
+        [mainmenuSpriteBatchNode addChild:ninja];
+        ninja.position = ccp(screenSize.width * 0.67f, screenSize.height * 0.46f);
+        
+        [self addChild:mainmenuSpriteBatchNode];
+        
+        [self displayMainMenu];
     }
     
     return self;
@@ -49,18 +54,18 @@
 }
 
 -(void)displayMainMenu {
-    CGSize screenSize = [CCDirector sharedDirector].winSize;
-    
-    // selector needs to run GameScene
-    CCMenuItemImage *playGameButton = [CCMenuItemImage itemWithNormalImage:@"PlayGameButtonNormal.png" selectedImage:@"PlayGameButtonSelected.png" disabledImage:nil target:self selector:@selector(playGameScene)];
-    
-    CCMenuItemImage *aboutUsButton = [CCMenuItemImage itemWithNormalImage:@"BuyBookButtonNormal.png" selectedImage:@"BuyBookButtonSelected.png" disabledImage:nil target:self selector:@selector(showAboutUs)];
-    
-    self.mainMenu = [CCMenu menuWithItems:playGameButton, aboutUsButton, nil];
-    [self.mainMenu alignItemsVerticallyWithPadding:screenSize.height * 0.02f];
-    self.mainMenu.position = ccp(screenSize.width/2, screenSize.height * 0.15f);
-    
-    [self addChild:self.mainMenu z:0 tag:kMainMenuTagValue];
+//    CGSize screenSize = [CCDirector sharedDirector].winSize;
+//    
+//    // selector needs to run GameScene
+//    CCMenuItemImage *playGameButton = [CCMenuItemImage itemWithNormalImage:@"PlayGameButtonNormal.png" selectedImage:@"PlayGameButtonSelected.png" disabledImage:nil target:self selector:@selector(playGameScene)];
+//    
+//    CCMenuItemImage *aboutUsButton = [CCMenuItemImage itemWithNormalImage:@"BuyBookButtonNormal.png" selectedImage:@"BuyBookButtonSelected.png" disabledImage:nil target:self selector:@selector(showAboutUs)];
+//    
+//    self.mainMenu = [CCMenu menuWithItems:playGameButton, aboutUsButton, nil];
+//    [self.mainMenu alignItemsVerticallyWithPadding:screenSize.height * 0.02f];
+//    self.mainMenu.position = ccp(screenSize.width/2, screenSize.height * 0.15f);
+//    
+//    [self addChild:self.mainMenu z:0 tag:kMainMenuTagValue];
 }
 
 @end
