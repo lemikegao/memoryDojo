d0791a3940b02b0dbc91a104c2391412f775b9c6
Michael Gao <mgao.ucla@gmail.com>
2012-12-24 15:41:53 +0800

    added back button to settings layer

--- memoryDojo/SettingsLayer.m
+++ memoryDojo/SettingsLayer.m
@@ -13,14 +13,26 @@
 -(id)init {
     self = [super init];
     if (self != nil) {
-        CCLOG(@"SettingsLayer->init");
+        CGSize screenSize = [CCDirector sharedDirector].winSize;
+        
         CCLayerColor *background = [CCLayerColor layerWithColor:ccc4(0, 0, 0, 255)];
         [self addChild:background];
         
         CCLabelBMFont *placeholder = [CCLabelBMFont labelWithString:@"SETTINGS LAYER" fntFile:@"grobold_30px_nostroke.fnt"];
-        CGSize screenSize = [CCDirector sharedDirector].winSize;
         placeholder.position = ccp(screenSize.width/2, screenSize.height/2);
         [background addChild:placeholder];
+        
+        // add back button
+        CCMenuItemLabel *backButton = [CCMenuItemLabel itemWithLabel:[CCLabelBMFont labelWithString:@"BACK" fntFile:@"grobold_21px_nostroke.fnt"] block:^(id sender) {
+            // move the settings layer back down
+            [self.mainMenuSceneDelegate hideSettings];
+        }];
+        backButton.anchorPoint = ccp(0, 1);
+        backButton.position = ccp(screenSize.width * 0.05f, screenSize.height * 0.95f);
+        
+        CCMenu *settingsMenu = [CCMenu menuWithItems:backButton, nil];
+        settingsMenu.position = CGPointZero;
+        [background addChild:settingsMenu];
     }
     
     return self;
